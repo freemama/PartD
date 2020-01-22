f71bdc29b367878c7de969632dc7b35af0f4d176
freemama <freeman.apou@xtra.co.nz>
2020-01-22 21:16:14 +1300

    Second commit: have added string labels and printed both.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -3,8 +3,14 @@ public class PartD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String name = "Freeman Apou";
+		String favCol = "Green";
+		String transport = "Car";
+		String dayOfWeek = "Friday, because the week is over";
 		
-		System.out.println("Hello, my name is __________");
+		System.out.println("Hello, my name is " + name);
+		System.out.println("My favourite colour is "+ favCol);
+		
 	
 		
 		
